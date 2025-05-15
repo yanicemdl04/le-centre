f2e2eca30efceb2a0e2d895c1299566c27027643
Yanice <yanicemundele@gmail.com>
2025-05-15 19:31:18 +0100

    modification pour host

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,14 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  base: '/le-centre.github.io/' // Supprimez `export default` et les accolades superflues
+  base: '/le-centre./',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        404: './public/404.html' // Ajoutez cette ligne
+      }
+    }
+  }
 });
